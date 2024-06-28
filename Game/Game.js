59f9e1c5d8a81214b5a107ad44cbe32eccfcd7b6
DiogoKovalek <diogokovalek@gmail.com>
2024-06-28 17:40:57 -0300

    v0.0.1
    teste

---
+++ Game/Game.js
@@ -0,0 +1,14 @@
+
+var config = {
+    width: 256,
+    height: 272,
+    bacgroundColor: 0x000000,
+    scene: [Scene1, Scene2],
+    pixelArt: true
+}
+
+var gameSettings = {
+    playerSpeed: 3
+}
+
+var game = new Phaser.Game(config);
---
+++ Scenes/Scene2.js
@@ -0,0 +1,75 @@
+
+class Scene2 extends Phaser.Scene {
+  constructor() {
+    super("playGame");
+  }
+
+    create(){
+        // iniciando Background ==============================
+        //this.background = this.add.image(0,0,"background");
+        this.background = this.add.tileSprite(0,0,config.width, config.height, "background");
+        this.background.setOrigin(0,0); // Ponto de referencia central da imagem, no caso superior esquerdo
+        // ===================================================
+
+        // iniciando Ships ===================================
+        /*
+        this.ship1 = this.add.image(config.width / 2 - 50, config.height / 2, "ship1");
+        this.ship2 = this.add.image(config.width/2, config.height/2, "ship2");
+        this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3");
+        */
+
+        this.player = this.add.sprite(config.width/2 - 8, config.height -64, "player");
+        this.player.play("player_anim");
+        
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
+
+
+        this.input.on("gameobjectdown", this.destroyShip, this);
+
+
+        /*
+        this.ship1.setScale(2);   //Escala de imagem
+        this.ship2.flipY = true;  //Espelhamento
+        this.ship3.angle += 45;   //Angulo em graus
+        */  
+
+        //======================================================
+
+        this.add.text(20,20, "Playing Game", {font: "25px Arial", fill:"yellow"});
+    }
+
+    update(){
+
+        this.background.tilePositionY -= 0.5;
+
+        this.movePlayerManager();
+    }
+
+    moveShip(ship, speed){
+        ship.y += speed
+        if(ship.y > config.height){
+            this.resetShipPos(ship);
+        }
+    }
+
+    resetShipPos(ship){
+        ship.y = 0;
+        var randomX = Phaser.Math.Between(0, config.width);
+        ship.x = randomX;
+    }
+
+    destroyShip(pointer, gameObject){
+        gameObject.setTexture("explosion");
+        gameObject.play("explode");
+    }
+
+    movePlayerManager(){
+        if(this.cursorKeys.left.isDown){
+            //this.player.setVelocityX(-gameSettings.playerSpeed);
+            this.player.x += -gameSettings.playerSpeed;
+        }else if(this.cursorKeys.right.isDown){
+            //this.player.setVelocityX(gameSettings.playerSpeed);
+            this.player.x += gameSettings.playerSpeed;
+        }
+    }
+}
